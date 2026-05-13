0c2fa273ba7e950b22df51de50ac36adca29bb7a
Igor-tsap <tsap.ihor@gmail.com>
2026-05-13 14:07:16 +0300

    ADD: alembic migration

---
+++ backend/alembic/versions/a2e73777dbbe_initial.py
@@ -0,0 +1,25 @@
+"""initial
+
+Revision ID: a2e73777dbbe
+Revises: 
+Create Date: 2026-05-13 10:03:57.138977
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import mysql
+
+# revision identifiers, used by Alembic.
+revision: str = 'a2e73777dbbe'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+def downgrade() -> None:
+    pass
