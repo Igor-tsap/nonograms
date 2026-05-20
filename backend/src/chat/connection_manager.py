fa9cc4837c860d9552673b471c5a7147d9f00dd4
Igor-tsap <tsap.ihor@gmail.com>
2026-05-20 18:15:12 +0300

    ADD: redis listener

--- backend/src/chat/connection_manager.py
+++ backend/src/chat/connection_manager.py
@@ -1,15 +1,22 @@
 from fastapi import WebSocket
+from database.redis import redis
+import asyncio
 
 
 class ConnectionManager:
     def __init__(self):
         self.connections = {}
+        self.listeners = set()
 
     async def connect(self, websocket: WebSocket, room_id: str):
         await websocket.accept()
         if room_id not in self.connections:
             self.connections[room_id] = set()
         self.connections[room_id].add(websocket)
+
+        if room_id not in self.listeners:
+            self.listeners.add(room_id)
+            asyncio.create_task(self.start_room_listener(room_id))
 
     def disconnect(self, websocket: WebSocket, room_id: str):
         if room_id in self.connections:   
@@ -19,7 +26,7 @@ class ConnectionManager:
                 if not self.connections[room_id]:
                     del self.connections[room_id]
 
-    async def broadcast(self, message: str, room_id: str):
+    async def broadcast_local(self, message: str, room_id: str):
         dead_connections = []
 
         for connection in self.connections.get(room_id, set()):
@@ -31,3 +38,10 @@ class ConnectionManager:
 
         for connection in dead_connections:
             self.disconnect(connection, room_id)
+
+    async def start_room_listener(self, room_id: str):
+        pubsub = redis.pubsub()
+        await pubsub.subscribe(f"puzzle_{room_id}")
+        async for message in pubsub.listen():
+            if message['type'] == 'message':
+                await self.broadcast_local(message['data'], room_id)
